762be5432cfd39a3c5521f1bd2f6d2d492754e42
artcheeze <surasak.jaikum@mail.kmutt.ac.th>
2018-11-08 10:00:59 +0700

    Example biz logic for BackEnd team

--- src/main/java/com/girls/TheGirl/Product/Product.java
+++ src/main/java/com/girls/TheGirl/Product/Product.java
@@ -35,6 +35,8 @@ public class Product {
     @NotNull
     private String prodBrand;
 
+   
+
     
     
 }
--- src/main/java/com/girls/TheGirl/Product/ProductController.java
+++ src/main/java/com/girls/TheGirl/Product/ProductController.java
@@ -6,9 +6,16 @@
 package com.girls.TheGirl.Product;
 
 import java.util.List;
+import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -21,10 +28,19 @@ public class ProductController {
     
     @Autowired
     ProductService ProductService;
-
+    
+    //////////////////////////////////////เรียกดูสินค้าทั้งหมด ดูได้ใน Service/////////////////////////////////////
     @GetMapping()
     public List<Product> getProduct() {
         return ProductService.retrieveProduct();
+    }
+    
+    //////////////////////////////////////เพิ่มสินค้าด้วย JSON ในรูปแบบของการยิง POST เข้ามา/////////////////////////////////////
+    @PostMapping()
+    public ResponseEntity<?> postCustomer(@Valid @RequestBody Product body){
+        Product p = ProductService.createProduct(body);
+        
+        return ResponseEntity.status(HttpStatus.CREATED).body(p);
     }
     
 }

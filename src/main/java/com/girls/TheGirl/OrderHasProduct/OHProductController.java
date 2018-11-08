608216fdf1b345c4d2deb2d8d4373c2e541bb999
artcheeze <surasak.jaikum@mail.kmutt.ac.th>
2018-11-08 11:56:59 +0700

    Bye bye hacker

--- src/main/java/com/girls/TheGirl/OrderHasProduct/OHProductController.java
+++ src/main/java/com/girls/TheGirl/OrderHasProduct/OHProductController.java
@@ -8,8 +8,12 @@ package com.girls.TheGirl.OrderHasProduct;
 import com.girls.TheGirl.Product.Product;
 import com.girls.TheGirl.OrderHasProduct.OHProductService;
 import java.util.List;
+import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,6 +31,11 @@ public class OHProductController {
     @GetMapping()
     public List<OHProduct> getProduct() {
         return OHProductService.retrieveOHProduct();
+    }
+    
+    public ResponseEntity<?> postCustomer(@Valid @RequestBody OHProduct body){
+        OHProduct ohp = OHProductService.createOHProduct(body);
+        return ResponseEntity.status(HttpStatus.CREATED).body(ohp);
     }
 
 }

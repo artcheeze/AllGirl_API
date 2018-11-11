5de352fc45168ad9ac6221a8ff7e44768f9772b7
Unknown <mattawan.meeraksa@mail.kmutt.ac.th>
2018-11-11 09:48:42 +0700

    create ProductProperties and add attribute

---
+++ src/main/java/com/girls/TheGirl/ProductProperties/ProductProperties.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.girls.TheGirl.Product;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.*;
+
+import lombok.Data;
+
+/**
+ *
+ * @author A
+ */
+@Data
+@Entity
+public class ProductProperties {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long prodPropId;
+
+    @NotBlank
+    private long prodDetailId;
+
+    private String prodSize;
+    private String prodColor;
+}

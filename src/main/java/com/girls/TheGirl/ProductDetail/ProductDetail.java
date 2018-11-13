7ec924cfce33ec30f39ea870800cf703bf284fc7
Unknown <mattawan.meeraksa@mail.kmutt.ac.th>
2018-11-13 16:38:35 +0700

    add joincolumn annotation and move prodImage,prodBrand to ProductDetail

--- src/main/java/com/girls/TheGirl/ProductDetail/ProductDetail.java
+++ src/main/java/com/girls/TheGirl/ProductDetail/ProductDetail.java
@@ -1,10 +1,13 @@
 package com.girls.TheGirl.ProductDetail;
 
+import com.girls.TheGirl.Product.Product;
 import java.io.Serializable;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
 import javax.validation.constraints.*;
 
 import lombok.Data;
@@ -12,14 +15,21 @@ import lombok.Data;
 @Data
 @Entity
 public class ProductDetail implements Serializable {
-
-    @NotBlank
     @Id
-    private long prodId;
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long prodDetailId;
+    
+    @NotNull
+    @OneToOne
+    @JoinColumn(name ="prodId")
+    private Product prodId;
 
     @NotNull
     private String prodCategory;
     private String prodDetail;
-
+    private String prodImage;
+    
+    @NotNull
+    private String prodBrand;
     private String prodProperties;
 }

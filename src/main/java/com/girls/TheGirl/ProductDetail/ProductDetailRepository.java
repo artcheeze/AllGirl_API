650fe354ce46a8727967d145f28371226bae6a1b
Unknown <mattawan.meeraksa@mail.kmutt.ac.th>
2018-11-14 00:14:44 +0700

    change crudrepo to jparepo

--- src/main/java/com/girls/TheGirl/ProductDetail/ProductDetailRepository.java
+++ src/main/java/com/girls/TheGirl/ProductDetail/ProductDetailRepository.java
@@ -5,10 +5,10 @@ import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.EmptyResultDataAccessException;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
 
-public interface ProductDetailRepository extends CrudRepository<ProductDetail, Long> {
+public interface ProductDetailRepository extends JpaRepository<ProductDetail, Long> {
 
     List<Product> findById(long prodId);
 

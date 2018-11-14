ac17c907b9b03d6e8e05be72aab698da8eebbde1
warunya chomchan <warunya.chch@mail.kmutt.ac.th>
2018-11-14 16:23:06 +0700

    add method PostMapping ProductPropertiesController

--- src/main/java/com/girls/TheGirl/ProductProperties/ProductPropertiesController.java
+++ src/main/java/com/girls/TheGirl/ProductProperties/ProductPropertiesController.java
@@ -5,14 +5,19 @@
  */
 package com.girls.TheGirl.ProductProperties;
 
+import com.girls.TheGirl.Product.Product;
 import com.girls.TheGirl.ProductDetail.ProductDetail;
 import com.girls.TheGirl.ProductDetail.ProductDetailService;
 import java.util.List;
 import java.util.Optional;
+import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -40,5 +45,12 @@ public class ProductPropertiesController {
             return ResponseEntity.notFound().build();
         }
         return ResponseEntity.ok(prodProperties);
+    }
+     
+    @PostMapping()
+    public ResponseEntity<?> postProductPropertiesController(@Valid @RequestBody Product body){
+        Product prod = productPropertiesService.createProduct(body);
+        
+        return ResponseEntity.status(HttpStatus.CREATED).body(prod);
     }
 }

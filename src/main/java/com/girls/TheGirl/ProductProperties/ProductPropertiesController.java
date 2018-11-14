e9effb29989c90745cb6ecf11bfed168aebcdd5a
Unknown <mattawan.meeraksa@mail.kmutt.ac.th>
2018-11-14 15:31:50 +0700

    update requestmapping to "/productproperties"

--- src/main/java/com/girls/TheGirl/ProductProperties/ProductPropertiesController.java
+++ src/main/java/com/girls/TheGirl/ProductProperties/ProductPropertiesController.java
@@ -22,7 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 
 @RestController
-@RequestMapping("/ProductProperties")
+@RequestMapping("/productproperties")
 public class ProductPropertiesController {
     
     @Autowired

68658c3fddc24609f89b3476d02ae14d9e4aeed0
Unknown <mattawan.meeraksa@mail.kmutt.ac.th>
2018-11-14 20:32:08 +0700

    update requestmapping("/Product")

--- src/main/java/com/girls/TheGirl/Product/ProductController.java
+++ src/main/java/com/girls/TheGirl/Product/ProductController.java
@@ -26,7 +26,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @author A
  */
 @RestController
-@RequestMapping("/product")
+@RequestMapping("/Product")
 public class ProductController {
     
     @Autowired
